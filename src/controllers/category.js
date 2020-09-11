f858a4c037758560706ffde9dbc17b10f5832089
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-11 09:25:08 +0100

    created validation on category controler using Joi

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -1,8 +1,9 @@
 const db = require('../helpers/db')
+const {categorySchema} = require('../helpers/validation_schema')
 
 module.exports = {
   createCategory: async (req, res) => {
-    const { id, name } = req.body
+    const {id,name} = await categorySchema.validate(req.body)
     
   }
 }
