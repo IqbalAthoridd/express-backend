7fbaa8ff11c317b93ceddbd0b5d732c0abad6029
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-12 16:31:47 +0100

    created registerSchema on validation_Schema

--- src/helpers/validation_schema.js
+++ src/helpers/validation_schema.js
@@ -4,6 +4,12 @@ const categorySchema = Joi.object({
   name: Joi.string().min(4).required()
 })
 
+const registerSchema = Joi.object({
+  name: Joi.string.min(4).max(20).required(),
+  email: Joi.string().lowercase().min(5).email().required(),
+  password: Joi.string().min(8).required()
+})
+
 module.exports = {
   categorySchema
 }
