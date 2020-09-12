7ee8253e369f6cb2120442ed2671cc773b50e05e
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-12 21:08:17 +0100

    creted query for get data cart on getCartModel

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -1,25 +1,38 @@
-const { createCartModel } = require('../models/cart')
+const { createCartModel, getCartItemModel } = require('../models/cart')
 
 module.exports = {
   createCart: (req, res) => {
-    const { userId, itemId } = req.body
+    const { userId, itemId, total } = req.body
 
-    createCartModel(+userId, +itemId, result => {
-      if (result.affectedRows > 0) {
-        res.send({
-          success: true,
-          message: 'Item added to cart',
-          data: {
-            id: result.insertId,
-            ...req.body
+    getCartItemModel(itemId, dataResult => {
+      if (dataResult.length === 0) {
+        dataResult = [{ itemId: 0 }]
+      }
+      if (dataResult[0].itemId !== +itemId) {
+        createCartModel(userId, itemId, total, result => {
+          if (result.affectedRows > 0) {
+            res.send({
+              success: true,
+              message: 'Item added to cart',
+              data: {
+                id: result.insertId,
+                ...req.body
+              }
+            })
+          } else {
+            res.send({
+              success: false,
+              message: 'Internal Server Error'
+            })
           }
         })
       } else {
         res.send({
           success: false,
-          message: 'Internal Server Error'
+          message: 'Item alredy added on cart'
         })
       }
     })
   }
+
 }
--- src/models/cart.js
+++ src/models/cart.js
@@ -1,10 +1,21 @@
 const db = require('../helpers/db')
 
 module.exports = {
-  createCartModel: (user, produk, cb) => {
-    db.query(`INSERT INTO cart (userId,itemId) VALUES (${user},${produk})`, (_err, result, _field) => {
+  createCartModel: (user, item, total, cb) => {
+    db.query(`INSERT INTO cart (userId,itemId,total) VALUES (${+(user)},${+item},${+total})`, (_err, result, _field) => {
       cb(result)
     })
+  },
+  getCartItemModel: (id, cb) => {
+    db.query(`SELECT * FROM cart WHERE itemId = ${id}`, (_err, result, _field) => {
+      cb(result)
+    })
+  },
+  getCartModel: (cb) => {
+    db.query('SELECT name, price, price * total as Summary FROM items INNER JOIN cart ON items.id = cart.itemId '
+      , (_err, result, _field) => {
+        cb(result)
+      })
   }
 
 }
