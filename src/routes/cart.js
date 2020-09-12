c3d6b06a3550f43b70f8c8028c92dd3c707a84bd
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-12 21:12:41 +0100

    creted get methord for getCart

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,8 +1,9 @@
 const { Router } = require('express')
 const router = Router()
 
-const { createCart } = require('../controllers/cart')
+const { createCart, getCart } = require('../controllers/cart')
 
 router.post('/', createCart)
+router.get('/', getCart)
 
 module.exports = router
