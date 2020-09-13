1c07704c4c6914d7fc388e9617b668113053aad7
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-13 14:09:09 +0100

    add updateTotal on cart.js models

--- src/models/cart.js
+++ src/models/cart.js
@@ -1,24 +1,29 @@
 const db = require('../helpers/db')
-
+const table = 'cart'
 module.exports = {
   createCartModel: (user, item, total, cb) => {
-    db.query(`INSERT INTO cart (userId,itemId,total) VALUES (${+(user)},${+item},${+total})`, (_err, result, _field) => {
+    db.query(`INSERT INTO ${table} (userId,itemId,total) VALUES (${+(user)},${+item},${+total})`, (_err, result, _field) => {
       cb(result)
     })
   },
   getCartItemModel: (id, cb) => {
-    db.query(`SELECT * FROM cart WHERE itemId = ${id}`, (_err, result, _field) => {
+    db.query(`SELECT * FROM ${table} WHERE itemId = ${id}`, (_err, result, _field) => {
       cb(result)
     })
   },
   getCartModel: (cb) => {
-    db.query('SELECT name, total, price, price * total as Summary FROM items INNER JOIN cart ON items.id = cart.itemId '
+    db.query(`SELECT name, total, price, price * total as Summary FROM items INNER JOIN ${table} ON items.id = cart.itemId `
       , (_err, result, _field) => {
         cb(result)
       })
   },
   deleteCartModel: (id, cb) => {
-    db.query(`DELETE FROM cart WHERE cartId = ${id}`, (_err, result, _field) => {
+    db.query(`DELETE FROM ${table} WHERE cartId = ${id}`, (_err, result, _field) => {
+      cb(result)
+    })
+  },
+  updateTotalModel: (id, total, cb) => {
+    db.query(`UPDATE ${table} SET total = ${total} WHERE cartId=${id}`, (_err, result, _field) => {
       cb(result)
     })
   }
