4712749f9227cdd3bd8d33d3e3d00abbeaa05417
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-13 12:40:59 +0100

    added string replace for all schema

--- src/helpers/validation_schema.js
+++ src/helpers/validation_schema.js
@@ -5,14 +5,14 @@ const categorySchema = Joi.object({
 })
 
 const registerSchema = Joi.object({
-  name: Joi.string().min(4).max(20).trim().required(),
+  name: Joi.string().min(4).max(20).trim().replace("'", "\'").required(),
   email: Joi.string().lowercase().min(5).trim().email().required(),
   password: Joi.string().min(8).trim().required(),
   phoneNumber: Joi.string().max(13).min(10).trim().required()
 })
 
 const updateSchema = Joi.object({
-  name: Joi.string().min(4).max(20).trim().required(),
+  name: Joi.string().min(4).max(20).trim().replace("'", "\'").required(),
   email: Joi.string().email().trim().lowercase().min(5).required(),
   phoneNumber: Joi.string().max(13).min(10).trim().required(),
   gender: Joi.string().trim(),
