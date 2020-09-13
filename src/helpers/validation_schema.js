e4c2f376f7bdf2bbb012c815de2582497f6687a0
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-13 11:42:54 +0100

    added trim() on categorySchema:

--- src/helpers/validation_schema.js
+++ src/helpers/validation_schema.js
@@ -1,7 +1,7 @@
 const Joi = require('@hapi/joi')
 
 const categorySchema = Joi.object({
-  name: Joi.string().min(4).required()
+  name: Joi.string().min(4).trim().required()
 })
 
 const registerSchema = Joi.object({
