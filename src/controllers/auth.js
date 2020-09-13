ab3a167e3dadd2b0d95c098d0f72f535519d3c3e
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-13 22:49:18 +0100

    added upload images system on auth Register

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,53 +1,56 @@
 const { registerSchema, updateSchema } = require('../helpers/validation_schema')
 const bcrypt = require('bcrypt')
+const { upload2 } = require('../helpers/init_multer')
 
 const { creteUserModel, getUserModel, updateUserModel } = require('../models/auth')
 
 module.exports = {
   authRegister: async (req, res) => {
-    try {
-      const data = await registerSchema.validateAsync({ ...req.body })
-
-      const salt = 10
-      data.password = await bcrypt.hash(data.password, salt)
-      getUserModel(data.email, dataResult => {
-        if (!dataResult.length) {
-          dataResult = [{ email: '', phoneNumber: '' }]
-        }
-        if (dataResult[0].phoneNumber === data.phoneNumber) {
-          res.send({
-            success: false,
-            message: 'Phone number already use'
-          })
-        }
-        if (dataResult[0].email !== data.email) {
-          creteUserModel(data, result => {
-            if (result.affectedRows > 0) {
-              res.send({
-                success: true,
-                message: 'User Created',
-                data: data
-              })
-            } else {
-              res.send({
-                success: false,
-                message: 'Interal Server Error'
-              })
-            }
-          })
-        } else {
-          res.send({
-            success: false,
-            message: `Email ${data.email} already use, try another email`
-          })
-        }
-      })
-    } catch (err) {
-      res.send({
-        success: false,
-        message: err.message
-      })
-    }
+    upload2(req, res, async (_err) => {
+      try {
+        const { filename } = req.file
+        const data = await registerSchema.validateAsync({ ...req.body })
+        const salt = 10
+        data.password = await bcrypt.hash(data.password, salt)
+        getUserModel(data.email, dataResult => {
+          if (!dataResult.length) {
+            dataResult = [{ email: '', phoneNumber: '' }]
+          }
+          if (dataResult[0].phoneNumber === data.phoneNumber) {
+            res.send({
+              success: false,
+              message: 'Phone number already use'
+            })
+          }
+          if (dataResult[0].email !== data.email) {
+            creteUserModel(data, filename, result => {
+              if (result.affectedRows > 0) {
+                res.send({
+                  success: true,
+                  message: 'User Created',
+                  data: data
+                })
+              } else {
+                res.send({
+                  success: false,
+                  message: 'Interal Server Error'
+                })
+              }
+            })
+          } else {
+            res.send({
+              success: false,
+              message: `Email ${data.email} already use, try another email`
+            })
+          }
+        })
+      } catch (err) {
+        res.send({
+          success: false,
+          message: err.message
+        })
+      }
+    })
   },
   authLogin: (req, res) => {
     const { email, password } = req.body
@@ -80,34 +83,44 @@ module.exports = {
     })
   },
   editUser: async (req, res) => {
-    try {
-      const { id } = req.params
-      let data = await updateSchema.validateAsync({ ...req.body })
-      data = Object.entries(data).map(data => {
-        return data[1] > 0 ? `${data[0]} = '${data[1]}'` : `${data[0]} = '${data[1]}'`
-      })
-
-      updateUserModel(id, data, result => {
-        if (result === undefined) {
-          result = { affectedRows: 0 }
-        }
-        if (result.affectedRows > 0) {
-          res.send({
-            success: true,
-            message: 'Data updated'
-          })
+    upload2(req, res, async (_err) => {
+      try {
+        const { id } = req.params
+        let image = ''
+        let coma = ''
+        if (req.file === undefined) {
+          image = ''
         } else {
-          res.send({
-            success: false,
-            message: 'Data not updated'
-          })
+          image = `image = "${req.file.filename}"`
+          coma = ','
         }
-      })
-    } catch (err) {
-      res.send({
-        success: false,
-        message: err.message
-      })
-    }
+        let data = await updateSchema.validateAsync({ ...req.body })
+        data = Object.entries(data).map(data => {
+          return data[1] > 0 ? `${data[0]} = '${data[1]}'` : `${data[0]} = '${data[1]}'`
+        })
+
+        updateUserModel(id, data, [image, coma], result => {
+          if (result === undefined) {
+            result = { affectedRows: 0 }
+          }
+          if (result.affectedRows > 0) {
+            res.send({
+              success: true,
+              message: 'Data updated'
+            })
+          } else {
+            res.send({
+              success: false,
+              message: 'Data not updated'
+            })
+          }
+        })
+      } catch (err) {
+        res.send({
+          success: false,
+          message: err.message
+        })
+      }
+    })
   }
 }
