87908c34e218efb449b255de55cfb778c1f029d9
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-13 15:39:46 +0100

    fixed some error on replace function on validationSchema

--- src/helpers/validation_schema.js
+++ src/helpers/validation_schema.js
@@ -1,19 +1,19 @@
 const Joi = require('@hapi/joi')
 
 const categorySchema = Joi.object({
-  name: Joi.string().min(4).trim().replace("'", "\'").required()
+  name: Joi.string().min(4).trim().replace("'", '\'').required()
 })
 
 const registerSchema = Joi.object({
-  name: Joi.string().min(4).max(20).trim().replace("'", "\'").required(),
-  email: Joi.string().lowercase().min(5).trim().email().replace("'", "\'").required(),
-  password: Joi.string().min(8).trim().replace("'", "\'").required(),
+  name: Joi.string().min(4).max(20).trim().replace("'", '\'').required(),
+  email: Joi.string().lowercase().min(5).trim().email().replace("'", '\'').required(),
+  password: Joi.string().min(8).trim().replace("'", '\'').required(),
   phoneNumber: Joi.string().max(13).min(10).trim().required()
 })
 
 const updateSchema = Joi.object({
-  name: Joi.string().min(4).max(20).trim().replace("'", "\'"),
-  email: Joi.string().email().trim().lowercase().min(5).replace("'", "\'"),
+  name: Joi.string().min(4).max(20).trim().replace("'", '\''),
+  email: Joi.string().email().trim().lowercase().min(5).replace("'", '\''),
   phoneNumber: Joi.string().max(13).min(10).trim(),
   gender: Joi.string().trim(),
   birthDay: Joi.string().trim()
