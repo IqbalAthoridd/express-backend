668ef5358c14f5dcbc56ce22276ff9c2d1c77fc5
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-13 13:27:43 +0100

    created UpdateUserModel on auth models

--- src/models/auth.js
+++ src/models/auth.js
@@ -1,14 +1,24 @@
 const db = require('../helpers/db')
-
+const table = 'users'
 module.exports = {
   creteUserModel: (data, cb) => {
-    db.query(`INSERT INTO users (name,email,password,phoneNumber) VALUES ('${data.name}','${data.email}','${data.password}','${data.phoneNumber}')`, (_err, result, _field) => {
+    db.query(`INSERT INTO ${table} (name,email,password,phoneNumber) VALUES ('${data.name}','${data.email}','${data.password}','${data.phoneNumber}')`, (_err, result, _field) => {
       console.log(_err)
       cb(result)
     })
   },
   getUserModel: (data, cb) => {
-    db.query(`SELECT * FROM users WHERE email = '${data}'`, (_err, result, _field) => {
+    db.query(`SELECT * FROM ${table} WHERE email = '${data}'`, (_err, result, _field) => {
+      cb(result)
+    })
+  },
+  getUserByidModel: (id, cb) => {
+    db.query(`SELECT * FROM ${table} WHERE userId = ${id}`, (_err, result, _field) => {
+      cb(result)
+    })
+  },
+  updateUserModel: (id, arr, cb) => {
+    db.query(`UPDATE ${table} SET ${arr} WHERE userId=${id}`, (_err, result, _field) => {
       cb(result)
     })
   }
