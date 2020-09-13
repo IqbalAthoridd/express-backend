5326837ecec9a483c72de8bf19b194745d6b0d5b
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-13 13:00:32 +0100

    fix some error

--- src/helpers/validation_schema.js
+++ src/helpers/validation_schema.js
@@ -1,20 +1,20 @@
 const Joi = require('@hapi/joi')
 
 const categorySchema = Joi.object({
-  name: Joi.string().min(4).trim().required()
+  name: Joi.string().min(4).trim().replace("'", "\'").required()
 })
 
 const registerSchema = Joi.object({
   name: Joi.string().min(4).max(20).trim().replace("'", "\'").required(),
-  email: Joi.string().lowercase().min(5).trim().email().required(),
-  password: Joi.string().min(8).trim().required(),
+  email: Joi.string().lowercase().min(5).trim().email().replace("'", "\'").required(),
+  password: Joi.string().min(8).trim().replace("'", "\'").required(),
   phoneNumber: Joi.string().max(13).min(10).trim().required()
 })
 
 const updateSchema = Joi.object({
-  name: Joi.string().min(4).max(20).trim().replace("'", "\'").required(),
-  email: Joi.string().email().trim().lowercase().min(5).required(),
-  phoneNumber: Joi.string().max(13).min(10).trim().required(),
+  name: Joi.string().min(4).max(20).trim().replace("'", "\'"),
+  email: Joi.string().email().trim().lowercase().min(5).replace("'", "\'"),
+  phoneNumber: Joi.string().max(13).min(10).trim(),
   gender: Joi.string().trim(),
   birthDay: Joi.string().trim()
 
