16c1ece8beddc157cad6a193f7bfc6150f0b4e4e
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-13 14:07:46 +0100

    created update cart

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,10 +1,11 @@
 const { Router } = require('express')
 const router = Router()
 
-const { createCart, getCart, deleteCart } = require('../controllers/cart')
+const { createCart, getCart, deleteCart, UpdateTotalItem } = require('../controllers/cart')
 
 router.post('/', createCart)
 router.get('/', getCart)
 router.delete('/:id', deleteCart)
+router.put('/:id', UpdateTotalItem )
 
 module.exports = router
