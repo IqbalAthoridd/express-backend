bae8f8e1940cd8d7d6f7ae922ae027bab89c7f14
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-13 09:50:32 +0100

    added phoneNumber column on query

--- src/models/auth.js
+++ src/models/auth.js
@@ -2,11 +2,10 @@ const db = require('../helpers/db')
 
 module.exports = {
   creteUserModel: (data, cb) => {
-    db.query(`INSERT INTO users (name,email,password) VALUES ('${data.name}','${data.email}','${data.password}')`
-      , (_err, result, _field) => {
-        console.log(_err)
-        cb(result)
-      })
+    db.query(`INSERT INTO users (name,email,password,phoneNumber) VALUES ('${data.name}','${data.email}','${data.password}','${data.phoneNumber}')`, (_err, result, _field) => {
+      console.log(_err)
+      cb(result)
+    })
   },
   getUserModel: (data, cb) => {
     db.query(`SELECT * FROM users WHERE email = '${data}'`, (_err, result, _field) => {
