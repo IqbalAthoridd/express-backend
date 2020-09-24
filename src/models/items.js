639875514669d9e8cc9ce60db4004f969e009221
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-24 07:56:36 +0100

    changed query on searchItem

--- src/models/items.js
+++ src/models/items.js
@@ -39,17 +39,23 @@ module.exports = {
       cb(result)
     })
   },
-  searchItemModel: (Search, arr, sort, sortTo, sortTime, price = 0, cb) => {
+  searchItemModel: (Search, arr, sort, sortTo, sortTime, price = 0) => {
     // db.query(`SELECT * FROM (SELECT * FROM ${table} WHERE ${Search[0]} LIKE "%${Search[1]}%" ${sort}
     // ORDER BY price ${sortTo} LIMIT ${arr[0]} OFFSET ${arr[1]}) AS tabel HAVING price >= ${price} ${sortTime} `
     // , (_err, result, _field) => {
     //   cb(result)
     // })
-    const rowSubQuery = 'i.id,i.name, i.price,i.description,c.name as Category,image1,image2,image3,image4,create_at,update_at'
-    const subQuery = `SELECT ${rowSubQuery} from ${table} i INNER JOIN imageitems on i.image=imageitems.imageId 
-    INNER JOIN category c on i.category = c.id WHERE i.${Search[0]} LIKE "%${Search[1]}%" ORDER BY create_at ${sortTo} LIMIT ${arr[0]} OFFSET ${arr[1]}`
-    db.query(`${subQuery}`, (_err, result, _field) => {
-      cb(result)
+    return new Promise((resolve, reject) => {
+      const rowSubQuery = 'i.id as id,i.name as name, i.price,i.description,c.name as category,image1,image2,image3,image4,create_at,update_at'
+      const subQuery = `SELECT ${rowSubQuery} from ${table} i INNER JOIN imageitems on i.image=imageitems.imageId 
+      INNER JOIN category c on i.category = c.id`
+      db.query(`SELECT * FROM (${subQuery}) as table_1 WHERE ${Search[0]} LIKE "%${Search[1]}%" ORDER BY create_at DESC LIMIT ${arr[0]} OFFSET ${arr[0]}`, (_err, result, _field) => {
+        if (_err) {
+          reject(_err)
+        } else {
+          resolve(result)
+        }
+      })
     })
   },
   countGetItemModel: (arr, sort, cb) => {
