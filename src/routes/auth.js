5222a743ded082239f899e59ad458d61e5733ca0
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-24 11:26:23 +0100

    added upload image middleware to register routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,10 @@
 const { Router } = require('express')
 const router = Router()
+const { upload } = require('../helpers/init_multer')
 
 const { authRegister, authLogin, editUser } = require('../controllers/auth')
 
-router.post('/register', authRegister)
+router.post('/register', upload.single('avatar',1), authRegister)
 router.post('/login', authLogin)
 router.patch('/myAccount/:id', editUser)
 
