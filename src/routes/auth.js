9c455f8b03a7d50d8bc256b01f23fdd9532b863d
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-26 10:22:31 +0100

    removed upload middleware

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,8 @@
 const { Router } = require('express')
 const router = Router()
-const { upload } = require('../helpers/init_multer')
-
 const { authRegister, authLogin, editUser } = require('../controllers/auth')
 
-router.post('/register', upload.single('avatar',1), authRegister)
+router.post('/register', authRegister)
 router.post('/login', authLogin)
 router.patch('/myAccount/:id', editUser)
 
