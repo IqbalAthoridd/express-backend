2a14d62731ffad76852395bd3026df7e2080b11e
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-26 10:26:17 +0100

    changed flow createUser

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,50 +1,54 @@
 const { registerSchema, updateSchema } = require('../helpers/validation_schema')
 const bcrypt = require('bcrypt')
 const { signAcessToken } = require('../middleware/auth')
+const { upload2 } = require('../helpers/init_multer')
 
 const { creteUserModel, getUserModel, updateUserModel } = require('../models/auth')
 
 module.exports = {
   authRegister: async (req, res) => {
-    try {
-      let { path } = req.file
-      path = path.replace(/\\/g, '/')
-      const { error, value } = await registerSchema.validate({ ...req.body })
-      if (error) {
-        res.send({
-          success: false,
-          message: error.details[0].message
-        })
-      }
-      const salt = 10
-      value.password = await bcrypt.hash(value.password, salt)
-      let dataResult = await getUserModel(value.email)
-      if (!dataResult.length) {
-        dataResult = [{ email: '' }]
-      }
-      if (dataResult[0].email !== value.email) {
-        const result = await creteUserModel(value, path)
-        if (result.affectedRows > 0) {
+    upload2(req, res, async (_err) => {
+      try {
+        let { path } = req.file
+        console.log(path)
+        path = path.replace(/\\/g, '/')
+        const { error, value } = await registerSchema.validate({ ...req.body })
+        if (error) {
           res.send({
-            success: true,
-            message: 'User Created',
-            data: { id: result.insertId, ...value, password: undefined, avatar: path }
+            success: false,
+            message: error.details[0].message
           })
+        }
+        const salt = 10
+        value.password = await bcrypt.hash(value.password, salt)
+        let dataResult = await getUserModel(value.email)
+        if (!dataResult.length) {
+          dataResult = [{ email: '' }]
+        }
+        if (dataResult[0].email !== value.email) {
+          const result = await creteUserModel(value, path)
+          if (result.affectedRows > 0) {
+            res.send({
+              success: true,
+              message: 'User Created',
+              data: { id: result.insertId, ...value, password: undefined, avatar: path }
+            })
+          } else {
+            res.send({
+              success: false,
+              message: 'Interal Server Error'
+            })
+          }
         } else {
           res.send({
             success: false,
-            message: 'Interal Server Error'
+            message: `Email ${value.email} already use, try another email`
           })
         }
-      } else {
-        res.send({
-          success: false,
-          message: `Email ${value.email} already use, try another email`
-        })
+      } catch (err) {
+        console.log(err)
       }
-    } catch (err) {
-      console.log(err)
-    }
+    })
   },
   authLogin: async (req, res) => {
     try {
