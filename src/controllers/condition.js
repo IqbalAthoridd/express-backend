9ddcbc45872e5aba2c265edd18c3b30dec988833
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-27 19:57:57 +0100

    created some function

--- src/controllers/condition.js
+++ src/controllers/condition.js
@@ -1,6 +1,6 @@
 const { conditionSchema } = require('../helpers/validation_schema')
 const { response } = require('../helpers/response')
-const { createData } = require('../helpers/database_query')
+const { createData, updateData, deleteDataById, listData } = require('../helpers/database_query')
 const table = 'conditions'
 module.exports = {
   createCondition: async (req, res) => {
@@ -13,5 +13,42 @@ module.exports = {
     } catch (error) {
 
     }
+  },
+  editCondition: async (req, res) => {
+    try {
+      const { id } = req.params
+      const data = await conditionSchema.validateAsync({ ...req.body })
+      const { affectedRows } = await updateData(table, id, data)
+      affectedRows
+        ? response(res, 'Condition updated')
+        : response(res, `Condition with id ${id} does't exist`, {}, false, 404)
+    } catch (error) {
+
+    }
+  },
+  deleteCondition: async (req, res) => {
+    try {
+      const { id } = req.params
+      const { affectedRows } = await deleteDataById(table, id)
+      affectedRows
+        ? response(res, 'Category deleted')
+        : response(res, `Condition with id ${id} does't exist`, {}, false, 404)
+    } catch (error) {
+
+    }
+  },
+  getCondition: async (req, res) => {
+    const { search = '', sortTo = 'ASC', limit = 5, page = 1 } = req.query
+    const offset = (page - 1) * limit
+    const data = [
+      [table],
+      ['%' + search + '%'],
+      [+limit],
+      [offset]
+    ]
+    const result = await listData(data, sortTo)
+    result.length
+      ? response(res, 'List of condition', { data: result })
+      : response(res, 'Data not found', {}, false, 404)
   }
 }
--- src/routes/condition.js
+++ src/routes/condition.js
@@ -1,6 +1,10 @@
 const { Router } = require('express')
 const router = Router()
 const condition = require('../controllers/condition')
+
 router.post('/', condition.createCondition)
+router.put('/:id', condition.editCondition)
+router.delete('/:id', condition.deleteCondition)
+router.get('/', condition.getCondition)
 
 module.exports = router
