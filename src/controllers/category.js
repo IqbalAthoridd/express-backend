b73b3cef9bcfdc4a9b636a8d4990bc175e13acb3
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-27 12:58:19 +0100

    changed getAllCategory to response standart and async/await function

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -1,15 +1,9 @@
-const qs = require('querystring')
 const { pagination } = require('../helpers/pagination')
 const { response } = require('../helpers/response')
 const { upload2 } = require('../helpers/init_multer')
 
 const { categorySchema } = require('../helpers/validation_schema')
-
-const {
-  getAllCategoryModel,
-  countGetCategoryModel
-} = require('../models/category')
-const { createData, getDataById, deleteDataById, updateData } = require('../helpers/database_query')
+const { createData, getDataById, deleteDataById, updateData, listData, countData } = require('../helpers/database_query')
 const table = 'categories'
 
 module.exports = {
@@ -55,51 +49,24 @@ module.exports = {
 
     }
   },
-  getAllCategory: (req, res) => {
-    let { search = '', limit = 5, page = 1 } = req.query
-
-    page = (page - 1) * limit
-    getAllCategoryModel(search, [limit, page], result => {
-      if (page === 0) {
-        page = 1
-      }
-      if (result) {
-        const pageInfo = {
-          count: 0,
-          pages: 0,
-          currentPage: page,
-          limitPerPage: +limit,
-          nextLink: null,
-          prevLink: null
-        }
-
-        if (result.length) {
-          countGetCategoryModel(search, data => {
-            const { count } = data[0]
-            pageInfo.count = count
-            pageInfo.pages = Math.ceil(count / limit)
-            const { pages, currentPage } = pageInfo
-            if (currentPage < pages) {
-              pageInfo.nextLink = `http://localhost:8080/category?${qs.stringify({ ...req.query, ...{ page: page + 1 } })}`
-            }
-            if (currentPage > 1) {
-              pageInfo.prevLink = `http://localhost:8080/category?${qs.stringify({ ...req.query, ...{ page: page - 1 } })}`
-            }
-            res.send({
-              success: true,
-              message: 'List of category',
-              data: result,
-              pageInfo
-            })
-          })
-        } else {
-          res.send({
-            success: false,
-            message: 'Data not found'
-          })
-        }
-      }
-    })
+  getAllCategory: async (req, res) => {
+    const { search = '', sortTo = 'ASC', limit = 5, page = 1 } = req.query
+    const offset = (page - 1) * limit
+    const data = [
+      [table],
+      ['%' + search + '%'],
+      [+limit],
+      [offset]
+    ]
+    const result = await listData(data, sortTo)
+    if (result.length) {
+      const data = await countData([[table], ['%' + search + '%']])
+      const { count } = data[0]
+      const pageInfo = await pagination(req.query, page, limit, count)
+      response(res, 'List of Category', { data: result, pageInfo })
+    } else {
+      response(res, 'Data not found', {}, false, 404)
+    }
   },
   updateCategory: async (req, res) => {
     try {
