74d406dc1c98c06b6d4bc7aeb8371b1bd16fad44
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-27 18:33:56 +0100

    created createCondition

--- src/controllers/condition.js
+++ src/controllers/condition.js
@@ -1,4 +1,17 @@
-
+const { conditionSchema } = require('../helpers/validation_schema')
+const { response } = require('../helpers/response')
+const { createData } = require('../helpers/database_query')
+const table = 'conditions'
 module.exports = {
+  createCondition: async (req, res) => {
+    try {
+      const data = await conditionSchema.validateAsync({ ...req.body })
+      const { affectedRows } = await createData(table, data)
+      affectedRows
+        ? response(res, 'Condition created!')
+        : response(res, 'Internal Server Error', {}, false, 500)
+    } catch (error) {
 
+    }
+  }
 }
