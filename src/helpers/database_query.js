b35ab961e6afeff1f1686801de79ad0a516c425e
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-27 10:03:10 +0100

    created GetDatabyID on databasequery

--- src/helpers/database_query.js
+++ src/helpers/database_query.js
@@ -4,8 +4,18 @@ const db = require('./db')
 module.exports = {
   createData: (table, data) => {
     return new Promise((resolve, reject) => {
-      db.query(`INSERT INTO ${table} SET ?`, data, (_err, result, _field) => {
+      db.query('INSERT INTO ?? SET ?', [table, data], (_err, result, _field) => {
         console.log(_err)
+        if (!_err) {
+          resolve(result)
+        }
+        reject(_err)
+      })
+    })
+  },
+  getDataById: (table, id) => {
+    return new Promise((resolve, reject) => {
+      db.query('SELECT * FROM ?? WHERE id = ?', [table, id], (_err, result, _field) => {
         if (!_err) {
           resolve(result)
         }
