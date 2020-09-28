a66fd224ba764d89f5689805dae36e2ea00d5c59
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-28 03:22:11 +0100

    created detailProduk public

--- src/controllers/public.js
+++ src/controllers/public.js
@@ -1,10 +1,23 @@
 const model = require('../models/public')
+const { getItemModel } = require('../models/items')
+const { response } = require('../helpers/response')
 
 module.exports = {
   newProducts: async (req, res) => {
     const data = await model.newProduct()
     if (data) {
       res.send({ succes: true, message: 'New Items', data: data })
+    }
+  },
+  detailProduct: async (req, res) => {
+    try {
+      const { id } = req.params
+      const result = await getItemModel(id)
+      result.length
+        ? response(res, 'Product', { data: result })
+        : response(res, 'internal server error', {}, false, 500)
+    } catch (error) {
+
     }
   }
 }
