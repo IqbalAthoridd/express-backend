dd57a59bd28083eb636297d5556a41bae3bb86c7
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-29 15:09:51 +0100

    added multer middleware on register route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,7 +3,7 @@ const router = Router()
 const { authRegister, authLogin, editUser } = require('../controllers/auth')
 const { upload } = require('../helpers/init_multer')
 
-router.post('/register', authRegister)
+router.post('/register', upload.single('avatar', 1), authRegister)
 router.post('/login', authLogin)
 router.patch('/myAccount/:id', upload.single('avatar', 1), editUser)
 
