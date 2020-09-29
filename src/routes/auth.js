efff486651da9818fe91685581fda9af36318e98
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-29 14:52:14 +0100

    changed multer middleware on auth route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,10 @@
 const { Router } = require('express')
 const router = Router()
 const { authRegister, authLogin, editUser } = require('../controllers/auth')
-const { upload3 } = require('../helpers/init_multer')
+const { upload } = require('../helpers/init_multer')
 
 router.post('/register', authRegister)
 router.post('/login', authLogin)
-router.patch('/myAccount/:id', upload3.single('avatar', 1), editUser)
+router.patch('/myAccount/:id', upload.single('avatar', 1), editUser)
 
 module.exports = router
