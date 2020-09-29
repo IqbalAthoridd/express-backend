cafcaa2490b314c836d3ed5e241047216c625d0d
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-29 14:52:40 +0100

    added multer middleware on category route

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,9 +1,10 @@
 const { Router } = require('express')
 const router = Router()
+const { upload } = require('../helpers/init_multer')
 
 const { createCategory, getCategory, deleteCategory, getAllCategory, updateCategory } = require('../controllers/category')
 
-router.post('/', createCategory)
+router.post('/', upload.single('avatar', 1), createCategory)
 router.get('/:id', getCategory)
 router.delete('/:id', deleteCategory)
 router.get('/', getAllCategory)
