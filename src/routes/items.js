11ce4d82ef5ef0e1c9c531755149272201e32e24
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-30 02:42:01 +0100

    added multer middleware on items route

--- src/routes/items.js
+++ src/routes/items.js
@@ -1,9 +1,10 @@
 const { Router } = require('express')
 const { getDetailItem, getItem, updateItem, updatePatrialItem, deleteItem, createItem } = require('../controllers/items')
+const { upload } = require('../helpers/init_multer')
 
 const router = Router()
 
-router.post('/', createItem)
+router.post('/', upload.array('picture'), createItem)
 router.get('/', getItem)
 router.get('/:id', getDetailItem)
 router.put('/:id', updateItem)
