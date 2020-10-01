04c020213aa293c4e74ae8b01e9afe7f8c0138f3
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-10-01 14:56:35 +0100

    created details route

--- src/routes/public.js
+++ src/routes/public.js
@@ -3,5 +3,6 @@ const router = Router()
 const controller = require('../controllers/public')
 
 router.get('/newItem', controller.newProducts)
+router.get('/details/:id', controller.detailProduct)
 
 module.exports = router
