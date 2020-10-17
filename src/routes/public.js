69e0fa9b00803fbd536b8e5b29b3bd82b276aafb
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-10-17 07:36:08 +0100

    added popular product

--- src/routes/public.js
+++ src/routes/public.js
@@ -3,6 +3,7 @@ const router = Router()
 const controller = require('../controllers/public')
 
 router.get('/newItem', controller.newProducts)
+router.get('/popular', controller.pupularProduct)
 router.get('/details/:id', controller.detailProduct)
 router.get('/category', controller.categoryList)
 router.get('/category/:name', controller.searchCategory)
