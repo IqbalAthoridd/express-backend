15d0c901f566bc59c8c2e975014cc2e7924677a5
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-10-17 11:17:13 +0100

    chaned db setting to ENV

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql')
+require('dotenv').config()
 
 const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'ecommerce'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password:process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 })
 conn.connect()
 module.exports = conn
