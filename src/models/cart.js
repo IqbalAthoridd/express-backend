919eaf8fac481e55569254e4b1e6e1f5b1c76f76
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-10-17 07:38:30 +0100

    added image and cateory name on querys cart

--- src/models/cart.js
+++ src/models/cart.js
@@ -2,9 +2,12 @@ const db = require('../helpers/db')
 module.exports = {
   getCartModel: (data) => {
     return new Promise((resolve, reject) => {
-      const subQuery = 'SELECT c.userId,p.name,p.price,c.picture,c.total,p.price*c.total as "summary" FROM carts c INNER JOIN products p ON c.productId = p.id'
+      const subQuery = `SELECT c.userId,p.name,k.name as category,p.price,c.total,i.url,p.price*c.total as "subTotal",i.indexOf 
+      FROM carts c INNER JOIN products p ON c.productId = p.id INNER JOIN product_picture i ON
+      p.id = i.productId INNER JOIN categories k on p.category_id = k.id WHERE i.indexOf = 0`
       db.query(`SELECT * FROM (${subQuery}) as table_1 WHERE ? LIMIT ? OFFSET ?`
         , data, (_err, result, _field) => {
+          console.log(_err)
           if (_err) {
             reject(_err)
           } else {
@@ -15,7 +18,7 @@ module.exports = {
   },
   countCartModel: (data) => {
     return new Promise((resolve, reject) => {
-      const subQuery = 'SELECT c.userId,p.name,p.price,c.picture,c.total FROM carts c INNER JOIN products p ON c.productId = p.id'
+      const subQuery = 'SELECT c.userId,p.name,p.price,c.total FROM carts c INNER JOIN products p ON c.productId = p.id'
       db.query(`SELECT COUNT('*') as count FROM (${subQuery}) as table_1 WHERE ?`,
         data, (_err, result, _field) => {
           if (_err) {
