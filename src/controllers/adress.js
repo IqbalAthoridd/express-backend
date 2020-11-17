c5d476de96b517bb7384da3dbe2f0897db1851f9
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-17 17:37:15 +0800

    added primary adress

--- src/controllers/adress.js
+++ src/controllers/adress.js
@@ -1,5 +1,5 @@
 const { response } = require('../helpers/response')
-const { createData, updateData, deleteDataById, getDataById, getDataByIdTwo } = require('../helpers/database_query')
+const { createData, updateData, deleteDataById, getDataByIdTwo, listAdress, updateDataPart } = require('../helpers/database_query')
 const { adreSchema } = require('../helpers/validation_schema')
 const table = 'user_adress'
 
@@ -30,11 +30,25 @@ module.exports = {
   },
   updatePartAdress: async (req, res) => {
     try {
+      const { userid } = req.payload
       const { id } = req.params
-      const { affectedRows } = await updateData(table, id, { ...req.body })
-      affectedRows
-        ? response(res, 'Adress updated', { data: { id, ...req.body } })
-        : response(res, 'Failed updated', {}, false, 400)
+      const { primary_adress } = req.body
+      console.log(primary_adress)
+      if (+primary_adress === 1) {
+        const dataPrimary = await getDataByIdTwo(table, { primary_adress: 1 }, { user_id: userid })
+        if (id !== dataPrimary[0].id) {
+          await updateDataPart(table, { id: dataPrimary[0].id }, { primary_adress: 0 })
+          const { affectedRows } = await updateData(table, id, { ...req.body })
+          affectedRows
+            ? response(res, 'Adress updated', { data: { id, ...req.body } })
+            : response(res, 'Failed updated', {}, false, 400)
+        }
+      } else {
+        const { affectedRows } = await updateData(table, id, { ...req.body })
+        affectedRows
+          ? response(res, 'Adress updated', { data: { id, ...req.body } })
+          : response(res, 'Failed updated', {}, false, 400)
+      }
     } catch (error) {
       error.isJoi === true && response(res, error.message, false, 400)
     }
@@ -51,11 +65,22 @@ module.exports = {
     }
   },
   getAdress: async (req, res) => {
-    const { userid } = req.payload
-    const result = await getDataById(table, { user_id: userid })
-    result.length
-      ? response(res, 'list adress', { data: result })
-      : response(res, 'You dont\'t have adress yet', {}, false, 400)
+    try {
+      const { limit = 3, page = 1 } = req.params
+      const offset = (page - 1) * limit
+      const { userid } = req.payload
+      const data = [
+        userid,
+        +limit,
+        +offset
+      ]
+      const result = await listAdress(data)
+      result.length
+        ? response(res, 'list adress', { data: result })
+        : response(res, 'You dont\'t have adress yet', {}, false, 400)
+    } catch (error) {
+      console.log(error)
+    }
   },
   getById: async (req, res) => {
     try {
@@ -67,7 +92,6 @@ module.exports = {
         ? response(res, 'Adress', { data: result })
         : response(res, 'Adress not found', {}, false, 404)
     } catch (error) {
-
     }
   }
 }
